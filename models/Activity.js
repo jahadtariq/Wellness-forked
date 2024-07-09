65b66e476edaa0aa6179333ad0f26d77f34311a2
jahadtariq <muhammadjahadt@gmail.com>
2024-07-09 17:12:49 +0500

    Made initial Activity log Schema

--- models/Activity.js
+++ models/Activity.js
@@ -1,11 +1,52 @@
 const mongoose = require('mongoose');
 
 const activitySchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true ,unique: true },
-    calories: { type: Number },
-    duration: { type: Number },
-    activity: { type: Number },
-    date: {type: Date}
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true ,
+        unique: true 
+    },
+    hydration: {
+        date: {
+            type: Date,
+            required: true,
+        },
+        progress: {
+            type: Number,
+            default: 0
+        }
+    },
+    smoking: {
+        date: {
+            type: Date,
+            required: true,
+        },
+        expense: {
+            type: Number,
+            default: 0
+        }
+    },
+    workouts: [
+        {
+          date: {
+            type: Date,
+            required: true
+          },
+          type: {
+            type: String,
+            required: true
+          },
+          duration: {
+            type: Number,
+            required: true
+          },
+          progress: {
+            type: Number,
+            default: 0
+          }
+        }
+    ]
 },
 {
     timestamps: true
