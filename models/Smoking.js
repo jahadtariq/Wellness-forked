4d5ef4b3fded872036365208a80c5da71eeacd3a
jahadtariq <muhammadjahadt@gmail.com>
2024-07-09 17:14:52 +0500

    Changed the schema of smoking service

--- models/Smoking.js
+++ models/Smoking.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const smokingSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true ,unique: true },
-    cigarettes: { type: Number },
+    noOfCigarettes: { type: Number },
+    totalExpense: {type: Number},
+    expense: {type:Number},
+    preferences: {
+        cigaretteName: String,
+        costPerCigarette: Number
+    }
 },
 {
     timestamps: true
