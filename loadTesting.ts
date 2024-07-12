ad0457b3cd485369c5b399900c0038974d51c421
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-07-12 11:18:51 -0700

    Made changes to load testing.ts

--- loadTesting.ts
+++ loadTesting.ts
@@ -1,23 +1,19 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+const http = require("k6/http");
+const { sleep } = require("k6");
 
 export const options = {
-
   // A number specifying the number of VUs to run concurrently.
   vus: 100,
   // A string specifying the total duration of the test run.
-  duration: '300s',
-
+  duration: "300s",
 };
 
-
-export default function() {
-
+export default function () {
   const params = {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  }
+  };
 
   for (let workoutId = 1; workoutId <= 100; workoutId++) {
     const url = `http://localhost:5000/api/schedule/workout/${workoutId}/completion`;
