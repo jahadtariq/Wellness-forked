27b526297aac02c267bf8480ca4bd808be1ce64a
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-07-12 09:43:11 -0700

    Ref: Getting ready to test the routers

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ connectToDatabase()
 
 //Express App Configurations
 const app = express();
-const PORT = process.env.PORT || 5000;
 app.use(helmet())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -30,29 +29,6 @@ app.use(SmokingRouter)
 app.use(UserRouter)
 app.use(WorkoutRouter)
 
-//Importing Controllers
-
-// User API's
-app.post('/user/post', newUser);
-
-// Medication API's
-app.post('/api/medication/post', newMeds);
-app.get("/api/medication/get/:userId", getMeds);
-
-// Hydration API's
-// app.post('/api/hydration/post', newHydration);
-app.post('/api/hydration/:id', updateHydration);
-app.get('/api/hydration/:id', getHydration);
-app.post('/api/preferences/:id', updatePreferences);
-
-// Smoking API's
-app.put('/api/smoking/:id', smokingApi)
-
-//Workout
-app.post('/api/schedule/workout/:userId', scheduleWorkout)
-app.put('/api/schedule/workout/:id/completion', workoutCompletion)
-//Get all workouts marked as scheduled
-app.get('/api/schedule/workout/:userId', getWorkouts)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
