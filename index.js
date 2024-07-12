166a0e1f78fc6612187fb3a3bd59e2472cfc3a3f
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-07-12 00:49:35 -0700

    Endpoints devloped

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const { getHydration } = require('./controllers/Hydration/getHydration');
 const { scheduleWorkout } = require('./controllers/Workout/scheduleWorkout');
 const { workoutCompletion } = require('./controllers/Workout/workoutCompletion');
 const { getWorkouts } = require('./controllers/Workout/getWorkouts');
+const Workout = require('./models/Workout');
+const { Mixed } = require('./models/Mixed');
 
 const app = express();
 
@@ -26,6 +28,53 @@ const PORT = process.env.PORT || 5000;
 connectToDatabase()
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+
+// GET random excercise length of limit.
+app.get('/exercises/:limit', async (req, res) => {
+    const limit = req.params.limit;
+    const excersises = await Mixed.find({}).limit(limit)
+    res.status(200).json({ data: excersises })
+
+});
+
+// GET searched results for specific body part
+app.get('/exercises/bodyPart/:bodyPart/:limit', async (req, res) => {
+    const bodyPart = req.params.bodyPart;
+    const limit = req.params.limit;
+    const excersises = await Mixed.find({ bodyPart: { $regex: "^" + bodyPart } }).limit(limit);
+    res.status(200).json({ data: excersises })
+});
+
+// GET /exercises/equipment/:type
+app.get('/exercises/equipment/:type/:limit', async (req, res) => {
+    const equipmentType = req.params.type;
+    const limit = req.params.limit;
+    const excersises = await Mixed.find({ equipment: { $regex: "^" + equipmentType } }).limit(limit);
+    res.status(200).json({ data: excersises })
+});
+
+
+// GET /exercises/name/:name
+app.get('/exercises/name/:name/:limit', async (req, res) => {
+    const exerciseName = req.params.name;
+    const limit = req.params.limit;
+    const excersises = await Mixed.find({ name: { $regex: "^" + exerciseName } }).limit(limit);
+    res.status(200).json({ data: excersises })
+});
+
+// GET /exercises/target/:target
+app.get('/exercises/target/:target/:limit', async (req, res) => {
+    const target = req.params.target;
+    const limit = req.params.limit;
+    const excersises = await Mixed.find({ target: { $regex: "^" + target } }).limit(limit);
+    res.status(200).json({ data: excersises })
+
+});
+
+
+
+
 
 // User API's
 app.post('/user/post', newUser);
@@ -47,7 +96,7 @@ app.put('/api/smoking/:id', smokingApi)
 app.post('/api/schedule/workout/:userId', scheduleWorkout)
 app.put('/api/schedule/workout/:id/completion', workoutCompletion)
 //Get all workouts marked as scheduled
-app.get('/api/schedule/workout/:userId',getWorkouts)
+app.get('/api/schedule/workout/:userId', getWorkouts)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
